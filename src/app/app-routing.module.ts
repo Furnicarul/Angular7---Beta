767dc9fca03f94668e600fbf88d6f4eaff87d9e2
Furnicarul <iulianschifirnet1@gmail.com>
2018-12-03 07:48:45 -0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ShopComponent } from './shop/shop.component';
+import { PricesComponent } from './prices/prices.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo:'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'sidebar',
+    component: SidebarComponent
+  },
+  {
+    path: 'shop',
+    component: ShopComponent
+  },
+  {
+    path: 'prices',
+    component: PricesComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ShopComponent } from './shop/shop.component';
+import { PricesComponent } from './prices/prices.component';
+import { HomeComponent } from './home/home.component';
+import { ShopDetailComponent } from './shop/shop-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ShopComponent,
+    PricesComponent,
+    HomeComponent,
+    ShopDetailComponent
   ],
   imports: [
     BrowserModule,
